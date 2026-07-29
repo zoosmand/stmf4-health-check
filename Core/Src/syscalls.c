d3b25f1c1cba0e8ef15eda3c31522a4b87490d32
Dmitry Slobodchikov <d.slobodchikov@intraclear.com>
2026-07-29 18:35:58 +0300

    Serialize RS485 output with a mutex

--- Core/Src/syscalls.c
+++ Core/Src/syscalls.c
@@ -22,8 +22,6 @@
 
 /* Includes */
 #include "rs485.h"
-#include "FreeRTOS.h"
-#include "task.h"
 
 #include <sys/stat.h>
 #include <stdlib.h>
@@ -90,18 +88,10 @@ __attribute__((weak)) int _write(int file, char *ptr, int len)
     return -1;
   }
 
-  BaseType_t schedulerRunning =
-    (xTaskGetSchedulerState() == taskSCHEDULER_RUNNING) ? pdTRUE : pdFALSE;
-  if (schedulerRunning == pdTRUE)
-    vTaskSuspendAll();
-
   HAL_StatusTypeDef status = Rs485_Transmit(
     (const uint8_t*)ptr,
     (size_t)len
   );
-
-  if (schedulerRunning == pdTRUE)
-    (void)xTaskResumeAll();
 
   if (status != HAL_OK) {
     errno = EIO;
